8c8b81d12cf328732f1563af8d37e1ee83f32936
jeonghwa Yoo <green2368@naver.com>
2021-01-11 21:08:53 +0900

    delete unnecessary files and add math equation/

--- work_module/nmf/exponential_nmf.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Sep  1 17:53:41 2020
-
-@author: 2020498
-"""
-
-
--- work_module/nmf/standard_nmf.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Sep  1 17:52:44 2020
-
-@author: 2020498
-"""
-
-
